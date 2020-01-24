e89610375d30236f8704e9550c2afdadc97f43cb
dmitrypasyutin <pasyutin84@gmail.com>
2020-01-24 18:31:09 +0700

    IMAGINGCLOUD-433: Updated SDK to include requests for image deskewing (#23)
    * Deskew image SDK
    
    * tests for deskew method added
    
    * SDK regenerated by CI server
    
    * deskew test imported
    
    * test fix
    
    * deskew djvu, dng, and dicom tests excluded
    
    * SDK regenerated by CI server
    
    * explanation for commented tests added
    
    * Revert "explanation for commented tests added"
    
    This reverts commit 6fc4fbe2420f2c8917c82f1458ab53d87e38bef5.
    
    * explanation for commented tests added
    
    Co-authored-by: aspose-imaging-cloud-deployer <37177976+aspose-imaging-cloud-deployer@users.noreply.github.com>

---
+++ asposeimagingcloud/models/requests/deskew_image_request.py
@@ -0,0 +1,114 @@
+#  coding: utf-8
+#  ----------------------------------------------------------------------------
+#  <copyright company="Aspose" file="deskew_image_request.py">
+#    Copyright (c) 2018-2019 Aspose Pty Ltd. All rights reserved.
+#  </copyright>
+#  <summary>
+#    Permission is hereby granted, free of charge, to any person obtaining a
+#   copy  of this software and associated documentation files (the "Software"),
+#   to deal  in the Software without restriction, including without limitation
+#   the rights  to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell  copies of the Software, and to permit persons to whom the
+#   Software is  furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all  copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#  </summary>
+#  ----------------------------------------------------------------------------
+
+from asposeimagingcloud.models.requests.imaging_request import ImagingRequest
+from asposeimagingcloud.models.requests.http_request import HttpRequest
+
+
+class DeskewImageRequest(ImagingRequest):
+    """
+    Request model for deskew_image operation.
+    Initializes a new instance.
+
+    :param name Image file name.
+    :param resize_proportionally Resize proportionally
+    :param bk_color Background color
+    :param folder Folder
+    :param storage Storage
+    """
+
+    def __init__(self, name, resize_proportionally, bk_color=None, folder=None, storage=None):
+        ImagingRequest.__init__(self)
+        self.name = name
+        self.resize_proportionally = resize_proportionally
+        self.bk_color = bk_color
+        self.folder = folder
+        self.storage = storage
+
+    def to_http_info(self, config):
+        """
+        Prepares initial info for HTTP request
+
+        :param config: Imaging API configuration
+        :type: asposeimagingcloud.Configuration
+        :return: http_request configured http request
+        :rtype: Configuration.models.requests.HttpRequest
+        """
+        # verify the required parameter 'name' is set
+        if self.name is None:
+            raise ValueError("Missing the required parameter `name` when calling `deskew_image`")
+        # verify the required parameter 'resize_proportionally' is set
+        if self.resize_proportionally is None:
+            raise ValueError("Missing the required parameter `resize_proportionally` when calling `deskew_image`")
+
+        collection_formats = {}
+        path = '/imaging/{name}/deskew'
+        path_params = {}
+        if self.name is not None:
+            path_params[self._lowercase_first_letter('name')] = self.name
+
+        query_params = []
+        if self._lowercase_first_letter('resizeProportionally') in path:
+            path = path.replace('{' + self._lowercase_first_letter('resizeProportionally' + '}'), self.resize_proportionally if self.resize_proportionally is not None else '')
+        else:
+            if self.resize_proportionally is not None:
+                query_params.append((self._lowercase_first_letter('resizeProportionally'), self.resize_proportionally))
+        if self._lowercase_first_letter('bkColor') in path:
+            path = path.replace('{' + self._lowercase_first_letter('bkColor' + '}'), self.bk_color if self.bk_color is not None else '')
+        else:
+            if self.bk_color is not None:
+                query_params.append((self._lowercase_first_letter('bkColor'), self.bk_color))
+        if self._lowercase_first_letter('folder') in path:
+            path = path.replace('{' + self._lowercase_first_letter('folder' + '}'), self.folder if self.folder is not None else '')
+        else:
+            if self.folder is not None:
+                query_params.append((self._lowercase_first_letter('folder'), self.folder))
+        if self._lowercase_first_letter('storage') in path:
+            path = path.replace('{' + self._lowercase_first_letter('storage' + '}'), self.storage if self.storage is not None else '')
+        else:
+            if self.storage is not None:
+                query_params.append((self._lowercase_first_letter('storage'), self.storage))
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = []
+
+        body_params = None
+
+        # HTTP header `Accept`
+        header_params['Accept'] = self._select_header_accept(
+            ['application/json'])
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = 'multipart/form-data' if form_params else self._select_header_content_type(
+            ['application/json'])
+
+        # Authentication setting
+        auth_settings = ['JWT']
+
+        return HttpRequest(path, path_params, query_params, header_params, form_params, body_params, local_var_files,
+                           collection_formats, auth_settings)
