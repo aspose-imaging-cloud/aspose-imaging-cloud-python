440cf2973ed918b1f7397a29fe5e7cddee23bede
Sergei Zubov <sergei.zubov@aspose.com>
2020-02-20 13:50:37 +0300

    Remove AI examples

--- Examples/asposeimagingcloudexamples/imaging_examples.py
+++ Examples/asposeimagingcloudexamples/imaging_examples.py
@@ -30,10 +30,6 @@ import sys
 
 from asposeimagingcloud import ImagingApi
 
-from asposeimagingcloudexamples.AI.compare_images import CompareImages
-from asposeimagingcloudexamples.AI.find_duplicate_images import FindDuplicateImages
-from asposeimagingcloudexamples.AI.find_similar_images import FindSimilarImages
-from asposeimagingcloudexamples.AI.search_images import SearchImages
 from asposeimagingcloudexamples.crop_image import CropImage
 from asposeimagingcloudexamples.export_image import ExportImage
 from asposeimagingcloudexamples.image_properties import ImageProperties
@@ -213,37 +209,6 @@ def main():
         wmf_image.modify_wmf_from_storage()
         wmf_image.modify_wmf_and_upload_to_storage()
         wmf_image.create_modified_wmf_from_request_body()
-
-        # AI APIs
-        print('Running AI examples:')
-        print()
-
-        # Compare two images
-        compare_images = CompareImages(api)
-        compare_images.prepare_search_context()
-        compare_images.compare_two_images_in_cloud()
-        compare_images.compare_loaded_image_to_image_in_cloud()
-        compare_images.delete_search_context()
-
-        # Find Duplicate Images
-        find_duplicate_images = FindDuplicateImages(api)
-        find_duplicate_images.prepare_search_context()
-        find_duplicate_images.find_image_duplicates()
-        find_duplicate_images.delete_search_context()
-
-        # Find Similar Images
-        find_similar_images = FindSimilarImages(api)
-        find_similar_images.prepare_search_context()
-        find_similar_images.find_similar_images()
-        find_similar_images.find_images_by_tag()
-        find_similar_images.search_image_from_web_source()
-        find_similar_images.delete_search_context()
-
-        # Search Images
-        search_images = SearchImages(api)
-        search_images.prepare_search_context()
-        search_images.search_image_from_web_source()
-        search_images.delete_search_context()
     except Exception as e:
         print('Something goes wrong: ' + str(e))
         sys.exit(1)
