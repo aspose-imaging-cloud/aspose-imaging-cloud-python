456167041a9d17dd15f4bb499f3d5027a8baab18
Sergei Zubov <sergei.zubov@aspose.com>
2020-03-10 16:04:57 +0300

    Add AI examples

---
+++ Examples/asposeimagingcloudexamples/AI/imaging_ai_base.py
@@ -0,0 +1,120 @@
+#  coding: utf-8
+#  ----------------------------------------------------------------------------
+#  <copyright company="Aspose" file="imaging_ai_base.py">
+#    Copyright (c) 2019 Aspose Pty Ltd. All rights reserved.
+#  </copyright>
+#  <summary>
+#    Permission is hereby granted, free of charge, to any person obtaining a
+#   copy  of this software and associated documentation files (the "Software"),
+#   to deal  in the Software without restriction, including without limitation
+#   the rights  to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell  copies of the Software, and to permit persons to whom the
+#   Software is  furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all  copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+
+#  </summary>
+#  ----------------------------------------------------------------------------
+
+import os
+import time
+
+import asposeimagingcloud.models.requests as requests
+
+
+class ImagingAiBase:
+    """Base class for AI examples"""
+    # The example images folder path
+    EXAMPLE_IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(
+        __file__)))), os.path.join('Images', 'AI'))
+
+    # The output folder path
+    OUTPUT_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))),
+                                 'Output')
+
+    # The cloud path
+    CLOUD_PATH = os.path.join('Examples', 'AI')
+
+    def __init__(self, imaging_api):
+        self._imaging_api = imaging_api
+        self._search_context_id = None
+
+    def delete_search_context(self):
+        """Deletes the image search context"""
+        folder = None  # File will be saved at the root of the storage
+        storage = None  # We are using default Cloud Storage
+
+        request = requests.DeleteImageSearchRequest(self._search_context_id, folder, storage)
+        self._imaging_api.delete_image_search(request)
+
+        print("Deleted new Search context with SearchContextId: {0}".format(self._search_context_id))
+        print()
+
+    def _create_search_context(self):
+        """Creates the image search"""
+        detector = 'akaze'
+        matching_algorithm = 'randomBinaryTree'
+        folder = None  # File will be saved at the root of the storage
+        storage = None  # We are using default Cloud Storage
+
+        request = requests.CreateImageSearchRequest(detector, matching_algorithm, folder, storage)
+        status = self._imaging_api.create_image_search(request)
+
+        self._search_context_id = status.id
+
+        print("Created new Search context with SearchContextId: {0}".format(self._search_context_id))
+        print()
+
+    def _create_image_features(self, source_path, is_folder):
+        """Extract images features and add them to search context"""
+        folder = None  # File will be saved at the root of the storage
+        storage = None  # We are using default Cloud Storage
+
+        request = requests.CreateImageFeaturesRequest(self._search_context_id, images_folder=os.path.join(
+            ImagingAiBase.CLOUD_PATH, source_path), folder=folder, storage=storage) if is_folder else \
+            requests.CreateImageFeaturesRequest(self._search_context_id, image_id=os.path.join(
+                ImagingAiBase.CLOUD_PATH, source_path), folder=folder, storage=storage)
+        self._imaging_api.create_image_features(request)
+
+        if is_folder:
+            print('Creating Search context image features...')
+            self._wait_idle(self._search_context_id)
+        else:
+            print("Created Search context image features for {0}".format(source_path))
+
+    def _upload_image_to_cloud(self, image_name, sub_folder=None):
+        """Uploads the image to cloud"""
+        folder = os.path.join(ImagingAiBase.EXAMPLE_IMAGES_FOLDER, sub_folder) if sub_folder else \
+            ImagingAiBase.EXAMPLE_IMAGES_FOLDER
+
+        local_input_image = os.path.join(folder, image_name)
+        request = requests.UploadFileRequest(os.path.join(ImagingAiBase.CLOUD_PATH, image_name), local_input_image)
+        self._imaging_api.upload_file(request)
+
+        print("Image {0} was uploaded to cloud storage".format(image_name))
+
+    @staticmethod
+    def _print_header(header):
+        print(header)
+        print()
+
+    def _wait_idle(self, search_context_id):
+        """Waits the idle"""
+        print('Waiting Search context Idle...')
+
+        folder = None  # File will be saved at the root of the storage
+        storage = None  # We are using default Cloud Storage
+
+        while self._imaging_api.get_image_search_status(requests.GetImageSearchStatusRequest(search_context_id,
+                                                                                             folder,
+                                                                                             storage)).search_status != 'Idle':
+            time.sleep(1)
--- Examples/asposeimagingcloudexamples/imaging_examples.py
+++ Examples/asposeimagingcloudexamples/imaging_examples.py
@@ -30,11 +30,14 @@ import sys
 
 from asposeimagingcloud import ImagingApi
 
+from asposeimagingcloudexamples.AI.compare_images import CompareImages
+from asposeimagingcloudexamples.AI.find_duplicate_images import FindDuplicateImages
+from asposeimagingcloudexamples.AI.find_similar_images import FindSimilarImages
 from asposeimagingcloudexamples.crop_image import CropImage
 from asposeimagingcloudexamples.deskew_image import DeskewImage
-from asposeimagingcloudexamples.grayscale_image import GrayscaleImage
 from asposeimagingcloudexamples.export_image import ExportImage
 from asposeimagingcloudexamples.filter_image import FilterImage
+from asposeimagingcloudexamples.grayscale_image import GrayscaleImage
 from asposeimagingcloudexamples.image_properties import ImageProperties
 from asposeimagingcloudexamples.imaging_base import ImagingBase
 from asposeimagingcloudexamples.resize_image import ResizeImage
@@ -229,6 +232,31 @@ def main():
         wmf_image.modify_wmf_from_storage()
         wmf_image.modify_wmf_and_upload_to_storage()
         wmf_image.create_modified_wmf_from_request_body()
+
+        # AI APIs
+        print('Running AI examples:')
+        print()
+
+        # Compare two images
+        compare_images = CompareImages(api)
+        compare_images.prepare_search_context()
+        compare_images.compare_two_images_in_cloud()
+        compare_images.compare_loaded_image_to_image_in_cloud()
+        compare_images.delete_search_context()
+
+        # Find Duplicate Images
+        find_duplicate_images = FindDuplicateImages(api)
+        find_duplicate_images.prepare_search_context()
+        find_duplicate_images.find_image_duplicates()
+        find_duplicate_images.delete_search_context()
+
+        # Find Similar Images
+        find_similar_images = FindSimilarImages(api)
+        find_similar_images.prepare_search_context()
+        find_similar_images.find_similar_images()
+        find_similar_images.find_images_by_tag()
+        find_similar_images.search_image_from_web_source()
+        find_similar_images.delete_search_context()
     except Exception as e:
         print('Something goes wrong: ' + str(e))
         sys.exit(1)
