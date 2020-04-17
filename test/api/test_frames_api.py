99617cd606cf789490d55a3c4521f294d147ddc7
Sergei Zubov <szubov@protonmail.com>
2020-04-17 11:19:49 +0300

    Update copyright date

--- test/api/test_frames_api.py
+++ test/api/test_frames_api.py
@@ -1,7 +1,7 @@
 #  coding: utf-8
 #  ----------------------------------------------------------------------------
 #  <copyright company="Aspose" file="test_frames_api.py">
-#    Copyright (c) 2019 Aspose Pty Ltd. All rights reserved.
+#    Copyright (c) 2019-2020 Aspose Pty Ltd. All rights reserved.
 #  </copyright>
 #  <summary>
 #    Permission is hereby granted, free of charge, to any person obtaining a
