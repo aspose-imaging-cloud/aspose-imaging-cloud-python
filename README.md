ae7c509c9cd5f4d92b9d11aad4633172d979348e
Alexander Ermakov <ermakoff.alexander@gmail.com>
2020-12-28 10:35:36 +0300

    Updated version to 20.12

--- README.md
+++ README.md
@@ -81,7 +81,7 @@ Detalied official documentation can be found at the [following link](https://doc
 
 Please, add the following [PyPi package](https://pypi.org/project/aspose-imaging-cloud/) to your requirements.txt.
 ```
-aspose-imaging-cloud>=20.11
+aspose-imaging-cloud>=20.12
 ```
 Or install it using command line.
 ```
--- asposeimagingcloud/api_client.py
+++ asposeimagingcloud/api_client.py
@@ -73,12 +73,12 @@ class ApiClient(object):
         self.pool = ThreadPool()
         self.rest_client = rest.RESTClientObject(configuration)
         self.default_headers = {'x-aspose-client': 'python sdk',
-                                'x-aspose-version': '20.11'}
+                                'x-aspose-version': '20.12'}
         if header_name is not None:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'python sdk 20.11'
+        self.user_agent = 'python sdk 20.12'
 
         self.last_response = None
 
