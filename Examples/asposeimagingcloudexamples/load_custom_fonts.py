24d82bf83bd86fde4f40321cd92e925ecb714c11
Alexander Ermakov <ermakoff.alexander@gmail.com>
2021-12-06 20:43:48 +0300

    Fixed error

--- Examples/asposeimagingcloudexamples/load_custom_fonts.py
+++ Examples/asposeimagingcloudexamples/load_custom_fonts.py
@@ -69,9 +69,8 @@ class LoadCustomFonts(ImagingBase):
     def _upload_fonts_to_cloud(self):
         """Uploads the font files to cloud"""
         fontsFolder = os.path.join(ImagingBase.EXAMPLE_IMAGES_FOLDER, "Fonts");  
-        for fontFile in glob.glob(glob(fontsFolder + "/*.ttf")):
-            local_input_font = os.path.join(fontsFolder, fontFile)
-            self._upload_image_to_cloud(os.path.join("Fonts", fontFile)), local_input_font)    
+        for fontFile in glob.glob(glob(fontsFolder + "/*.ttf")):           
+            self._upload_image_to_cloud(os.path.join("Fonts", fontFile)), os.path.join(fontsFolder, fontFile))    
 
     def _upload_file_to_cloud(self, file_name, file):
         """Uploads the file to cloud"""
